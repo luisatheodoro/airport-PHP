8c872c96650592067b3135614b4aab704c3deaea
Luisa Paoliello <luisa.paoliello@avadolearning.com>
2019-03-22 16:15:55 +0000

    get weather and returns bool for isStormy function

--- src/Weather.php
+++ src/Weather.php
@@ -1,9 +1,15 @@
 <?php
 
 class Weather {
-    public function randomWeather() :string {
+    private function randomWeather() :string {
         $forecasts = ['Sunny', 'Sunny', 'Sunny', 'Stormy'];
         $random_index = array_rand($forecasts);
         return $forecasts[$random_index];
+    }
+    private function getWeather() :string {
+        return $this->randomWeather();
+    }
+    public function isStormy() :bool {
+        return $this->getWeather() === 'Stormy';
     }
 }
