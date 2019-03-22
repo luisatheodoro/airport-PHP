d33c48593c88172d6a10bae967c265c5f172f7fc
Luisa Paoliello <luisa.paoliello@avadolearning.com>
2019-03-22 17:02:29 +0000

    plane can land

--- src/Plane.php
+++ src/Plane.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'Airport.php';
 class Plane {
 
     public function __construct($name) {
@@ -8,5 +8,12 @@ class Plane {
         $this->landed = false;
     }
 
-
+    public function land($airport) {
+        if ($this->landed) {
+            throw new Exception('Cannot land, because plane is already landed');
+        }elseif ($airport->canLand($this->name)){
+            $this->isLandedAt[] = $airport;
+            $this->landed = true;
+        }
+    }
 }
