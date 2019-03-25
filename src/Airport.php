9f4ebb199e7d3342f5301a997fafc1b274635e08
Luisa Paoliello <luisa.paoliello@avadolearning.com>
2019-03-25 12:23:02 +0000

    airport can instruct to takeoff

--- src/Airport.php
+++ src/Airport.php
@@ -3,7 +3,15 @@ declare(strict_types=1);
 
 final class Airport
 {
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
     public function canLand($plane=null) :bool {
+        return true;
+    }
+
+    public function canTakeOff($plane=null) :bool {
         return true;
     }
 }
